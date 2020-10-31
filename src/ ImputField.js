36e9079f7ff940d58883d1da5d09368d09c07ccd
mpsujek <mateusz.psujek@gmail.com>
2020-10-31 22:45:45 +0100

    Added imput component

---
+++ src/ ImputField.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+class ImputField extends React.Component {
+  constructor(props) {
+    super(props);
+    // create a ref to store the textInput DOM element
+    this.textInput = React.createRef();
+    this.focusTextInput = this.focusTextInput.bind(this);
+  }
+
+  focusTextInput() {
+    // Explicitly focus the text input using the raw DOM API
+    // Note: we're accessing "current" to get the DOM node
+    this.textInput.current.focus();
+    console.log(this.textInput.current.value);
+  }
+
+  render() {
+    // tell React that we want to associate the <input> ref
+    // with the `textInput` that we created in the constructor
+    return (
+      <div>
+        <input type="text" ref={this.textInput} />
+        <input
+          type="button"
+          value="Focus the text input"
+          onClick={this.focusTextInput}
+        />
+      </div>
+    );
+  }
+}
+
+export default ImputField;
